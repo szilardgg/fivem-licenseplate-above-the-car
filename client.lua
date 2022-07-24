8c4347dab8cbe9600c627eb9b00c5b80fd2b660e
szilardgg <91017007+szilardgg@users.noreply.github.com>
2022-07-24 15:41:21 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,5 +1,6 @@
 local show = true
 
+
 function DrawText3D(x, y, z, text)
     SetTextScale(0.50, 0.50)
     SetTextFont(4)
@@ -12,30 +13,38 @@ function DrawText3D(x, y, z, text)
     SetDrawOrigin(x,y,z, 0)
     DrawText(0.0, 0.0)
     DrawRect(0.0, 0.0+0.0165, 0.060, 0.033, 0, 0, 0, 60)
-    DrawRect(0.0, 0.0+0.0165, 0.057, 0.03, 0, 0, 0, 60)
+   -- DrawRect(0.0, 0.0+0.0165, 0.057, 0.03, 0, 0, 0, 60)
     ClearDrawOrigin()
 end
 
-Citizen.CreateThread(function()
-    while true do 
-        local waitTime = 500
-        local vehicles = GetGamePool("CVehicle")
-        local playerPed = PlayerPedId()
-        for i=1, #vehicles, 1 do
-            local vehCoords = GetEntityCoords(vehicles[i])
-            local distance = #(GetEntityCoords(playerPed) - vehCoords)
 
-            if distance < Config.distance and show then
-                waitTime = 1
-                local plate = GetVehicleNumberPlateText(vehicles[i])
+
+CreateThread(function()
+    while true do 
+    if show then
+        local vehicles = GetGamePool("CVehicle")
+    
+        for i=1, #vehicles, 1 do
+         
+       
+            local vehCoords = GetEntityCoords(vehicles[i])
+            local playerCoords = GetEntityCoords(PlayerPedId())
+            local distance = GetDistanceBetweenCoords(playerCoords, vehCoords[1], vehCoords[2], vehCoords[3], true)
+            local plate = GetVehicleNumberPlateText(vehicles[i])
+         
+            if distance < Config.distance and not IsPedInVehicle(PlayerPedId(),vehicles[i],true) then
+
                 DrawText3D(vehCoords[1], vehCoords[2], vehCoords[3]+Config.height, Config.country..plate)
             end
-        end
-
-        Citizen.Wait(waitTime)
+       
+    end
+    end
+      Wait(7)
     end
 end)
+
 
 RegisterCommand(Config.togCommand, function()
     show = not show   
 end)
+
