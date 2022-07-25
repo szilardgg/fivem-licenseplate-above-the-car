2b39aaa0ebb78fc617583a699317ed228f135af9
Csoki <feketetibi71@gmail.com>
2022-07-25 14:55:00 +0200

    some refactor and update to lua 5.4

--- client.lua
+++ client.lua
@@ -7,37 +7,35 @@ function DrawText3D(x, y, z, text)
     SetTextColour(255, 255, 255, 255)
     SetTextEntry("STRING")
     SetTextCentre(true)
-    SetTextOutline(0,0,0,255)
+    SetTextOutline(0, 0, 0, 255)
     AddTextComponentString(text)
-    SetDrawOrigin(x,y,z, 0)
+    SetDrawOrigin(x, y, z, 0)
     DrawText(0.0, 0.0)
     DrawRect(0.0, 0.0+0.0165, 0.060, 0.033, 0, 0, 0, 60)
-    -- DrawRect(0.0, 0.0+0.0165, 0.057, 0.03, 0, 0, 0, 60)
     ClearDrawOrigin()
 end
 
-CreateThread(function()
-    while true do 
-        if show then
-            local vehicles = GetGamePool("CVehicle")
-        
-            for i=1, #vehicles, 1 do
-                local vehCoords = GetEntityCoords(vehicles[i])
-                local playerCoords = GetEntityCoords(PlayerPedId())
-                local distance = GetDistanceBetweenCoords(playerCoords, vehCoords[1], vehCoords[2], vehCoords[3], true)
-                local plate = GetVehicleNumberPlateText(vehicles[i])
-            
-                if distance < Config.distance and not IsPedInVehicle(PlayerPedId(),vehicles[i],true) then
+function drawPlates()
+    while show do 
+        local vehicles <const> = GetGamePool("CVehicle")
+        local playerCoords <const> = GetEntityCoords(PlayerPedId())
 
-                    DrawText3D(vehCoords[1], vehCoords[2], vehCoords[3]+Config.height, Config.country..plate)
-                end
+        for i=1, #vehicles, 1 do
+            local vehCoords <const> = GetEntityCoords(vehicles[i])
+            
+            if #(playerCoords - vehCoords) < Config.distance and not IsPedInVehicle(PlayerPedId(),vehicles[i],true) then
+                local plate <const> = GetVehicleNumberPlateText(vehicles[i])
+                DrawText3D(vehCoords[1], vehCoords[2], vehCoords[3]+Config.height, Config.country .. plate)
             end
         end
-        Wait(7)
     end
-end)
+end
+CreateThread(drawPlates)
 
 RegisterCommand(Config.togCommand, function()
-    show = not show   
+    show = not show
+    if (show) then 
+        CreateThread(drawPlates)
+    end
 end)
 
