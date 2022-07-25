2d06864ad2867f9dfbea65daf6139945b26e3157
Csoki <feketetibi71@gmail.com>
2022-07-25 14:51:31 +0200

    format code

--- client.lua
+++ client.lua
@@ -1,6 +1,5 @@
 local show = true
 
-
 function DrawText3D(x, y, z, text)
     SetTextScale(0.50, 0.50)
     SetTextFont(4)
@@ -13,36 +12,30 @@ function DrawText3D(x, y, z, text)
     SetDrawOrigin(x,y,z, 0)
     DrawText(0.0, 0.0)
     DrawRect(0.0, 0.0+0.0165, 0.060, 0.033, 0, 0, 0, 60)
-   -- DrawRect(0.0, 0.0+0.0165, 0.057, 0.03, 0, 0, 0, 60)
+    -- DrawRect(0.0, 0.0+0.0165, 0.057, 0.03, 0, 0, 0, 60)
     ClearDrawOrigin()
 end
 
-
-
 CreateThread(function()
     while true do 
-    if show then
-        local vehicles = GetGamePool("CVehicle")
-    
-        for i=1, #vehicles, 1 do
-         
-       
-            local vehCoords = GetEntityCoords(vehicles[i])
-            local playerCoords = GetEntityCoords(PlayerPedId())
-            local distance = GetDistanceBetweenCoords(playerCoords, vehCoords[1], vehCoords[2], vehCoords[3], true)
-            local plate = GetVehicleNumberPlateText(vehicles[i])
-         
-            if distance < Config.distance and not IsPedInVehicle(PlayerPedId(),vehicles[i],true) then
+        if show then
+            local vehicles = GetGamePool("CVehicle")
+        
+            for i=1, #vehicles, 1 do
+                local vehCoords = GetEntityCoords(vehicles[i])
+                local playerCoords = GetEntityCoords(PlayerPedId())
+                local distance = GetDistanceBetweenCoords(playerCoords, vehCoords[1], vehCoords[2], vehCoords[3], true)
+                local plate = GetVehicleNumberPlateText(vehicles[i])
+            
+                if distance < Config.distance and not IsPedInVehicle(PlayerPedId(),vehicles[i],true) then
 
-                DrawText3D(vehCoords[1], vehCoords[2], vehCoords[3]+Config.height, Config.country..plate)
+                    DrawText3D(vehCoords[1], vehCoords[2], vehCoords[3]+Config.height, Config.country..plate)
+                end
             end
-       
-    end
-    end
-      Wait(7)
+        end
+        Wait(7)
     end
 end)
-
 
 RegisterCommand(Config.togCommand, function()
     show = not show   
